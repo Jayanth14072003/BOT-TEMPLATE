d66101fff84048dff9243b3af7c67b655995e6af
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2024-01-10 21:30:52 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,12 +5,11 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-import os
+import os, time
 import asyncio
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, CallbackQuery
-
 from bot import Bot
 from config import *
 from Script import script
@@ -18,15 +17,9 @@ from Script import script
 @Bot.on_message(filters.command('start') & filters.private)
 async def start_command(client: Client, message: Message):
     a = await message.reply_text("Processing....")
-    await delete(a)
+    await a.delete()
     await client.send_message(
-            text = script.START_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-            disable_web_page_preview = True,
-            quote = True
+        	chat_id=message.chat.id,
+            text = script.START_MSG.format(message.from_user.mention if message.from_user else message.chat.title),
+            disable_web_page_preview = True
         )
